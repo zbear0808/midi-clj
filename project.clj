004bd6dfc18879abb43f0c87cc0c4b4889c03e34
Jeff Rose <rosejn@gmail.com>
2010-08-24 16:44:04 +0200

    Moving to clojure 1.2, and expanding midi map.  Maybe this should move to defrecord though...

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject overtone/midi-clj "0.0.2-SNAPSHOT"
+(defproject overtone/midi-clj "0.2.0-SNAPSHOT"
   :description "A high level midi library to play notes and interact with external midi devices."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]])
+                 [org.clojure/clojure-contrib "1.2.0"]])
 
