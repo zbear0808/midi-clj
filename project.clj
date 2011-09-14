af5c93a8b297d2f8f012a9e3625a411c00c983c8
Sam Aaron <samaaron@gmail.com>
2011-09-14 14:05:25 +0100

    bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject overtone/midi-clj "0.1"
+(defproject overtone/midi-clj "0.2.0"
   :description "A high level midi library to play notes and interact with external midi devices."
   :dependencies [[org.clojure/clojure "1.3.0-RC0"]])
