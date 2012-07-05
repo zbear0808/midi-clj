52f399cf18463c72439cccb2cfb6c61135e579c4
Jeff Rose <rosejn@gmail.com>
2012-07-05 18:12:30 +0200

    Incrementing project version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/midi-clj "0.4.0"
-  :description "A high level midi library to play notes and interact with external midi devices."
+(defproject overtone/midi-clj "0.5.0"
+  :description "A high level midi library to read files, play notes, and interact with external midi devices."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [overtone/at-at "1.0.0"]])
