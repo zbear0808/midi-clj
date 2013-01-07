146f4b7fac75af2131ed2a82794db1bccb2cd5da
Sam Aaron <samaaron@gmail.com>
2013-01-07 19:41:47 +0000

    Correction, prepare for a 0.5.0 release!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/midi-clj "0.6.0"
+(defproject overtone/midi-clj "0.5.0"
   :description "A high level midi library to read files, play notes, and
                 interact with external midi devices."
   :dependencies [[org.clojure/clojure "1.3.0"]
